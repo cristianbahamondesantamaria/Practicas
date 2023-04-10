26dfb8b976ee3deee787cb4dc28e7d8bea092d35
Cristian Bahamonde <cristian.bahamonde.santamaria@gmail.com>
2023-04-10 11:35:46 +0200

    Terminado detalles entidad productos

--- js/detallesEntidad.js
+++ js/detallesEntidad.js
@@ -61,8 +61,46 @@ function mostrarPersonas() {
     }
 }
 
+function mostrarProductos() {
+    const detalles = JSON.parse(localStorage.getItem("entidadDetalles"));
+    const productos = JSON.parse(localStorage.getItem("productos"));
+    const divPrincipal = $('#Productos');
+    for (var i = 0; i < productos.length; i++) {
+        var entidadessize=productos[i].entidades.length;
+        for(var j=0; j<entidadessize; j++){
+            if (productos[i].entidades[j] === detalles.nombre) {
+                let cartaDiv = $('<div class="card d-flex align-items-center justify-content-center m-5 " style="max-width: 540px;"></div>');
+                let row = $('<div class="row g-0"></div>');
+    
+                let colomleft = $('<div class="col-md-4"></div>');
+                let imagen = $('<img class="img-fluid rounded-start" src="' + productos[i].imagen + '">');
+                colomleft.append(imagen);
+    
+                let colomright = $('<div class="col-md-8"></div>');
+                let cartaBody = $('<div class="card-body"></div>');
+                let titulo = $('<h5 class="card-title">' + productos[i].nombre + '</h5>');
+                let detalles = $('<button class="btn btn-primary">Ir a ' + productos[i].nombre + '</button>');
+                var producto=productos[i];
+                detalles.click(function () {
+                    localStorage.setItem("productoDetalles", JSON.stringify(producto));
+                    window.location.href = "detallesProducto.html";
+                });
+                cartaBody.append(titulo);
+                cartaBody.append(detalles);
+                colomright.append(cartaBody);
+                row.append(colomleft);
+                row.append(colomright);
+                cartaDiv.append(row);
+                divPrincipal.append(cartaDiv);
+            }
+        }
+        
+    }
+}
+
 function mostrarDetalles() {
     mostrarEntidad();
     mostrarPersonas();
+    mostrarProductos();
 }
 window.addEventListener("load", mostrarDetalles);
